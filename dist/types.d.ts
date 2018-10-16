ad0ab8c01df36a5c49a969bee7e9de4f78943b62
Gil Goldshlager <gil@coda.io>
2018-10-16 15:11:59 -0700

    Make build

--- dist/types.d.ts
+++ dist/types.d.ts
@@ -203,6 +203,7 @@ export interface PackDefinition {
     defaultAuthentication?: Authentication;
     exampleImages?: string[];
     exampleVideoIds?: string[];
+    gettingStartedImage?: string;
     /**
      * If specified, this pack requires system credentials to be set up via Coda's admin console in order to work when no
      * explicit connection is specified by the user.
