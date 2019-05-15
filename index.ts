e82dbfef044203aeb761d128212deb2718aa502b
Alex DeNeui <alexd@coda.io>
2019-05-15 16:49:03 -0700

    Fix missing export

--- index.ts
+++ index.ts
@@ -47,6 +47,7 @@ export {UserVisibleError} from './api';
 export {isArrayType} from './api_types';
 export {isObjectPackFormula} from './api';
 export {isStringPackFormula} from './api';
+export {isSyncPackFormula} from './api';
 export {isUserVisibleError} from './api';
 export {makeUserVisibleError} from './api';
 
