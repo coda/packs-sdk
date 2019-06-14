e33971faa8460a90d74d2d3c56145aa1b7d5d47f
Alex DeNeui <alexd@coda.io>
2019-06-14 10:42:17 -0700

    Build

--- dist/samples/npm.js
+++ dist/samples/npm.js
@@ -111,7 +111,7 @@ const FakeNpmDefinitionFake = {
                             return result.body;
                         })),
                     }),
-                    api_1.makeBooleanParameter('monthly', 'Show monthly download count instead of weekly', { optional: true, defaultValue: () => false }),
+                    api_1.makeBooleanParameter('monthly', 'Show monthly download count instead of weekly', { optional: true, defaultValue: true }),
                 ],
                 network: { hasSideEffect: false, hasConnection: false },
                 execute: ([name, monthly], context) => __awaiter(this, void 0, void 0, function* () {
