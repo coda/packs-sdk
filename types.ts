03e4a192b7e0ed02b2ef2e4abb7d10572116089a
Matt Hudson <matt@coda.io>
2019-09-24 19:36:18 -0700

    Add provider and pack id

--- types.ts
+++ types.ts
@@ -80,6 +80,7 @@ export enum PackId {
   CodaDoc = 1054,
   GoogleSheets = 1055,
   GoogleDocs = 1056,
+  ModeAnalytics = 1057,
 }
 
 export enum ProviderId {
@@ -125,6 +126,7 @@ export enum ProviderId {
   PubNub = 2040,
   Atlassian = 2041,
   Barcode = 2042,
+  ModeAnalytics = 2043,
 }
 
 export enum AuthenticationType {
