4778d6d862b85baf9d2f6ebbf2e66ddf2030f0e7
Christopher Eck <chris@coda.io>
2020-04-24 14:17:54 -0700

    Update CustomHeaderTokenAuthentication to allow the pack to specify the token prefix optionally

--- types.ts
+++ types.ts
@@ -222,11 +222,12 @@ export interface CodaApiBearerTokenAuthentication extends BaseAuthentication {
 
 /**
  * A pack or formula which uses standard bearer token header authentication:
- * {"Authorization": "Bearer <token here>"}
+ * {"HeaderNameHere": "OptionalTokenPrefixHere <token here>"}
  */
 export interface CustomHeaderTokenAuthentication extends BaseAuthentication {
   type: AuthenticationType.CustomHeaderToken;
   headerName: string;
+  tokenPrefix?: string;
 }
 
 /**
