e911e41f9269ef32e677d9cfcc619feda042ccd3
Alan Fang <alan@coda.io>
2021-01-21 15:49:06 -0600

    make formulaNamespace optional

--- dist/types.d.ts
+++ dist/types.d.ts
@@ -202,7 +202,7 @@ export interface PackDefinition {
         [featureSet in FeatureSet]: Quota;
     }>;
     rateLimits?: RateLimits;
-    formulaNamespace: string;
+    formulaNamespace?: string;
     /**
      * If specified, this pack requires system credentials to be set up via Coda's admin console in order to work when no
      * explicit connection is specified by the user.
