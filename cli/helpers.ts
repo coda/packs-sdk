9751ab153c70073e56e54b3ba7e96a3d11f3d3e3
Alan Fang <alan@coda.io>
2021-02-22 21:04:47 -0600

    cleanup

--- cli/helpers.ts
+++ cli/helpers.ts
@@ -1,14 +1,7 @@
 import {spawnSync} from 'child_process';
 
 export function spawnProcess(command: string) {
-  let cmd = command;
-  // Hack to allow us to run this CLI tool for testing purposes from within this repo, without
-  // needing it installed as an npm package.
-  if (process.argv[1].endsWith('coda.ts')) {
-    cmd = command.replace('coda-packs-sdk/dist', process.env.PWD!);
-  }
-
-  return spawnSync(cmd, {
+  return spawnSync(command, {
     shell: true,
     stdio: 'inherit',
   });
--- dist/cli/helpers.js
+++ dist/cli/helpers.js
@@ -3,13 +3,7 @@ Object.defineProperty(exports, "__esModule", { value: true });
 exports.spawnProcess = void 0;
 const child_process_1 = require("child_process");
 function spawnProcess(command) {
-    let cmd = command;
-    // Hack to allow us to run this CLI tool for testing purposes from within this repo, without
-    // needing it installed as an npm package.
-    if (process.argv[1].endsWith('coda.ts')) {
-        cmd = command.replace('coda-packs-sdk/dist', process.env.PWD);
-    }
-    return child_process_1.spawnSync(cmd, {
+    return child_process_1.spawnSync(command, {
         shell: true,
         stdio: 'inherit',
     });
