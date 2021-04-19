308578265890cca65097ec3b7ce5760480b32550
Betty Huang <42978433+betty-codaio@users.noreply.github.com>
2021-04-19 18:14:51 -0400

    Export compilePackMetadata (#935)

--- cli/publish.ts
+++ cli/publish.ts
@@ -1,31 +1,13 @@
 import type {AllPacks} from './create';
 import type {Arguments} from 'yargs';
-import type {Authentication} from '../types';
-import type {AuthenticationMetadata} from '../compiled_types';
-import {AuthenticationType} from '../types';
 import {ConsoleLogger} from '../helpers/logging';
-import type {Format} from '../types';
-import type {GenericSyncTable} from '../api';
-import type {MetadataFormula} from '../api';
-import type {MetadataFormulaMetadata} from '../api';
-import type {PackDefinition} from '../types';
-import type {PackFormatMetadata} from '../compiled_types';
-import type {PackFormulaMetadata} from '../api';
-import type {PackFormulas} from '../api';
-import type {PackFormulasMetadata} from '../compiled_types';
 import type {PackMetadata} from '../compiled_types';
-import type {PackSyncTable} from '../compiled_types';
 import type {PackUpload} from '../compiled_types';
-import type {PostSetup} from '../types';
-import type {PostSetupMetadata} from '../compiled_types';
-import type {TypedPackFormula} from '../api';
-import type {TypedStandardFormula} from '../api';
 import {build} from './build';
+import {compilePackMetadata} from '../helpers/cli';
 import {createCodaClient} from './helpers';
-import {ensureExists} from '../helpers/ensure';
 import {formatEndpoint} from './helpers';
 import {getApiKey} from './helpers';
-import {isDynamicSyncTable} from '../api';
 import {isTestCommand} from './helpers';
 import {printAndExit} from '../testing/helpers';
 import {readFile} from '../testing/helpers';
@@ -111,107 +93,4 @@ async function uploadPackToSignedUrl(bundleFilename: string, metadata: PackMetad
   } catch (err) {
     printAndExit(`Error in uploading Pack to signed url: ${err}`);
   }
-}
-
-function compilePackMetadata(manifest: PackDefinition): PackMetadata {
-  const {formats, formulas, formulaNamespace, syncTables, defaultAuthentication, ...definition} = manifest;
-  const compiledFormats = compileFormatsMetadata(formats || []);
-  const compiledFormulas = (formulas && compileFormulasMetadata(formulas)) || (Array.isArray(formulas) ? [] : {});
-  const defaultAuthenticationMetadata = compileDefaultAuthenticationMetadata(defaultAuthentication);
-  const metadata: PackMetadata = {
-    ...definition,
-    defaultAuthentication: defaultAuthenticationMetadata,
-    formulaNamespace,
-    formats: compiledFormats,
-    formulas: compiledFormulas,
-    syncTables: (syncTables || []).map(compileSyncTable),
-  };
-
-  return metadata;
-}
-
-function compileFormatsMetadata(formats: Format[]): PackFormatMetadata[] {
-  return formats.map(format => {
-    return {
-      ...format,
-      matchers: (format.matchers || []).map(matcher => matcher.toString()),
-    };
-  });
-}
-
-function compileFormulasMetadata(
-  formulas: PackFormulas | TypedStandardFormula[],
-): PackFormulasMetadata | PackFormulaMetadata[] {
-  const formulasMetadata: PackFormulaMetadata[] | PackFormulasMetadata = Array.isArray(formulas) ? [] : {};
-  // TODO: @alan-fang delete once we move packs off of PackFormulas
-  if (Array.isArray(formulas)) {
-    (formulasMetadata as PackFormulaMetadata[]).push(...formulas.map(compileFormulaMetadata));
-  } else {
-    for (const namespace of Object.keys(formulas)) {
-      (formulasMetadata as PackFormulasMetadata)[namespace] = formulas[namespace].map(compileFormulaMetadata);
-    }
-  }
-
-  return formulasMetadata;
-}
-
-function compileFormulaMetadata(formula: TypedPackFormula): PackFormulaMetadata {
-  const {execute, ...rest} = formula;
-  return rest;
-}
-
-function compileSyncTable(syncTable: GenericSyncTable): PackSyncTable {
-  if (isDynamicSyncTable(syncTable)) {
-    const {getter, getName, getSchema, getDisplayUrl, listDynamicUrls, ...rest} = syncTable;
-    const {execute, ...getterRest} = getter;
-    return {
-      ...rest,
-      getName: compileMetadataFormulaMetadata(getName),
-      getSchema: compileMetadataFormulaMetadata(getSchema),
-      getDisplayUrl: compileMetadataFormulaMetadata(getDisplayUrl),
-      listDynamicUrls: compileMetadataFormulaMetadata(listDynamicUrls),
-      getter: getterRest,
-    };
-  }
-
-  const {getter, ...rest} = syncTable;
-  const {execute, ...getterRest} = getter;
-  return {
-    ...rest,
-    getter: getterRest,
-  };
-}
-
-function compileDefaultAuthenticationMetadata(
-  authentication: Authentication | undefined,
-): AuthenticationMetadata | undefined {
-  if (!authentication) {
-    return;
-  }
-  if (authentication.type === AuthenticationType.None) {
-    return authentication;
-  }
-  const {getConnectionName, getConnectionUserId, postSetup, ...rest} = authentication;
-  return {
-    ...rest,
-    getConnectionName: compileMetadataFormulaMetadata(getConnectionName),
-    getConnectionUserId: compileMetadataFormulaMetadata(getConnectionUserId),
-    postSetup: postSetup ? postSetup.map(compilePostSetupStepMetadata) : undefined,
-  };
-}
-
-function compileMetadataFormulaMetadata(formula: MetadataFormula | undefined): MetadataFormulaMetadata | undefined {
-  if (!formula) {
-    return;
-  }
-  const {execute, ...rest} = formula;
-  return rest;
-}
-
-function compilePostSetupStepMetadata(step: PostSetup): PostSetupMetadata {
-  const {getOptionsFormula, ...rest} = step;
-  return {
-    ...rest,
-    getOptionsFormula: ensureExists(compileMetadataFormulaMetadata(getOptionsFormula)),
-  };
 }
---
+++ dist/helpers/cli.d.ts
@@ -0,0 +1,3 @@
+import type { PackDefinition } from '../types';
+import type { PackMetadata } from '../compiled_types';
+export declare function compilePackMetadata(manifest: PackDefinition): PackMetadata;
