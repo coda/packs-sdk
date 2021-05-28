aa8f5553aa472c81c4a0838532a22b5eda68bb6b
Patrick Barry <patrick@coda.io>
2021-05-28 10:56:00 -0700

    address review feedback

--- cli/helpers.ts
+++ cli/helpers.ts
@@ -2,6 +2,7 @@ import type {Authentication} from '../types';
 import {Client} from '../helpers/external-api/coda';
 import type {PackVersionDefinition} from '../types';
 import path from 'path';
+import {print} from '../testing/helpers';
 import {spawnSync} from 'child_process';
 
 export function spawnProcess(command: string) {
@@ -46,16 +47,16 @@ export function spawnBootstrapCommand(command: string) {
 }
 
 // Packs today do not have both defaultAuth and systemAuth specs, so this helper gets
-// whichever is available. This could in theory be supported in the future, for a use
-// case like a Google Maps pack which allowed a default credential from the pack author
-// to be used up to some rate limit, after which a power user would need to connect their
-// own Maps API credential.
+// whichever is available, defaulting to defaultAuth. A smarter version could be supported
+// in the future, for a use case like a Google Maps pack which allowed a default credential
+// from the pack author to be used up to some rate limit, after which a power user would need
+// to connect their own Maps API credential.
 export function getPackAuth(packDef: PackVersionDefinition): Authentication | undefined {
   const {defaultAuthentication, systemConnectionAuthentication} = packDef;
   if (defaultAuthentication && systemConnectionAuthentication) {
-    throw new Error(
-      'We do not support using both defaultAuthentication and systemConnectionAuthentication in the CLI.',
-    );
+    print('Both defaultAuthentication & systemConnectionAuthentication are specified.');
+    print('Using defaultAuthentication.');
+    return defaultAuthentication;
   }
   // Since SystemAuthentication is a strict subset of Authentication, we can cast them together.
   return defaultAuthentication || (systemConnectionAuthentication as Authentication);
