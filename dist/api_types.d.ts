c94d96c58c194bc92577de5d9aad469c4c59c73b
Patrick Barry <patrick@coda.io>
2021-06-17 11:56:32 -0700

    wow changing a comment changes the gen files

--- dist/api_types.d.ts
+++ dist/api_types.d.ts
@@ -131,7 +131,10 @@ export interface CommonPackFormulaDef<T extends ParamDefs> {
      */
     readonly isSystem?: boolean;
     /**
-     *
+     * OAuth scopes that the formula needs that weren't requested in the pack's overall authentication
+     * config. For example, a Slack pack can have one formula that needs admin privileges, but non-admins
+     * can use the bulk of the pack without those privileges. Coda will give users help in understanding
+     * that they need additional authentication to use a formula with extra OAuth scopes.
      */
     readonly requiredOAuthScopes?: string[];
 }
