14a63769773a4cf412ce37962214b856842a4976
Jonathan Goldman <50341122+jonathan-codaio@users.noreply.github.com>
2021-07-28 18:06:02 -0700

    Export makeReferenceSchemaFromObjectSchema (#1243)

--- dist/schema.d.ts
+++ dist/schema.d.ts
@@ -184,5 +184,12 @@ export declare const PlaceholderIdentityPackId = -1;
 export declare function makeObjectSchema<K extends string, L extends string, T extends ObjectSchemaDefinition<K, L>>(schemaDef: T): ObjectSchema<K, L>;
 export declare function normalizeSchemaKey(key: string): string;
 export declare function normalizeSchema<T extends Schema>(schema: T): T;
+/**
+ * Convenience for creating a reference object schema from an existing schema for the
+ * object. Copies over the identity, id, and primary from the schema, and the subset of
+ * properties indicated by the id and primary.
+ * A reference schema can always be defined directly, but if you already have an object
+ * schema it provides better code reuse to derive a reference schema instead.
+ */
 export declare function makeReferenceSchemaFromObjectSchema(schema: GenericObjectSchema, identityName?: string): GenericObjectSchema;
 export {};
