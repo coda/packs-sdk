2a626969b25abc2544fababcfcb6bf46ee1f40f5
Huayang Guo <70549840+huayang-codaio@users.noreply.github.com>
2021-08-02 17:38:44 -0700

    skip stack translation if something goes wrong (#1250)

--- runtime/common/source_map.ts
+++ runtime/common/source_map.ts
@@ -9,34 +9,45 @@ export async function translateErrorStackFromVM({
   vmFilename,
 }: {
   stacktrace?: string;
-  bundleSourceMapPath: string,
-  vmFilename: string,
+  bundleSourceMapPath: string;
+  vmFilename: string;
 }): Promise<string | undefined> {
   if (!stacktrace) {
     return stacktrace;
   }
 
-  const consumer = await new SourceMapConsumer(fs.readFileSync(bundleSourceMapPath, 'utf8'));
-  const stack = stackTraceParser.parse(stacktrace);
+  try {
+    const consumer = await new SourceMapConsumer(fs.readFileSync(bundleSourceMapPath, 'utf8'));
+    const stack = stackTraceParser.parse(stacktrace);
 
-  const translatedStack = stack.map(frame => {
-    if (frame.file !== vmFilename || frame.lineNumber === null || frame.column === null) {
-      return frame;
-    }
+    const translatedStack = stack.map(frame => {
+      if (frame.file !== vmFilename || frame.lineNumber === null || frame.column === null) {
+        return frame;
+      }
 
-    const originalFrame = consumer.originalPositionFor({line: frame.lineNumber, column: frame.column - 1});
-    
-    return {
-      ...frame,
-      file: originalFrame.source,
-      column: originalFrame.column ? originalFrame.column + 1 : originalFrame.column,
-      lineNumber: originalFrame.line,
-      methodName: originalFrame.name || frame.methodName,
-    };
-  });
+      const originalFrame = consumer.originalPositionFor({line: frame.lineNumber, column: frame.column - 1});
 
-  
-  return translatedStack.map(stackValue => 
-    `    at ${stackValue.methodName || '<unknown>'} (${stackValue.file}:${stackValue.lineNumber}:${stackValue.column})\n`
-  ).join('');
+      return {
+        ...frame,
+        file: originalFrame.source,
+        column: originalFrame.column ? originalFrame.column + 1 : originalFrame.column,
+        lineNumber: originalFrame.line,
+        methodName: originalFrame.name || frame.methodName,
+      };
+    });
+
+    return translatedStack
+      .map(
+        stackValue =>
+          `    at ${stackValue.methodName || '<unknown>'} (${stackValue.file}:${stackValue.lineNumber}:${
+            stackValue.column
+          })\n`,
+      )
+      .join('');
+  } catch (err) {
+    // something went wrong. just return the original stacktrace.
+    // eslint-disable-next-line no-console
+    console.log('Failed to translate error stack', err);
+    return stacktrace;
+  }
 }
