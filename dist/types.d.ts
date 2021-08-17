27fc5aece260e13d8c10b05126fc67567dbc7266
John Li <johnzli1995@gmail.com>
2021-08-17 15:11:42 -0700

    Make category optional for legacy packs

--- dist/types.d.ts
+++ dist/types.d.ts
@@ -230,7 +230,7 @@ export interface PackDefinition extends PackVersionDefinition {
     shortDescription: string;
     description: string;
     permissionsDescription?: string;
-    category: PackCategory;
+    category?: PackCategory;
     logoPath: string;
     enabledConfigName?: string;
     exampleImages?: string[];
