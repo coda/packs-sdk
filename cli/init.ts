2823d258a3c01e30652e5fd70363a50ec5aa6f4d
Jonathan Goldman <50341122+jonathan-codaio@users.noreply.github.com>
2021-10-13 17:18:59 -0700

    Explicitly create a .gitignore file in coda init, because npm is dumb. (#1457)
    * Explicitly create a .gitignore file in coda init, because npm is dumb.
    
    * Build.

--- cli/init.ts
+++ cli/init.ts
@@ -1,7 +1,11 @@
 import fs from 'fs';
 import {spawnProcess} from './helpers';
 
-const PACKS_EXAMPLES_DIRECTORY = 'node_modules/@codahq/packs-examples';
+const PacksExamplesDirectory = 'node_modules/@codahq/packs-examples';
+
+const GitIgnore = `.coda.json
+.coda-credentials.json
+`;
 
 export async function handleInit() {
   let isPacksExamplesInstalled: boolean;
@@ -18,15 +22,20 @@ export async function handleInit() {
     spawnProcess(installCommand);
   }
 
-  const packageJson = JSON.parse(fs.readFileSync(`${PACKS_EXAMPLES_DIRECTORY}/package.json`, 'utf-8'));
+  const packageJson = JSON.parse(fs.readFileSync(`${PacksExamplesDirectory}/package.json`, 'utf-8'));
   const devDependencies = packageJson.devDependencies;
   const devDependencyPackages = Object.keys(devDependencies)
     .map(dependency => `${dependency}@${devDependencies[dependency]}`)
     .join(' ');
   spawnProcess(`npm install --save-dev ${devDependencyPackages}`);
 
-  const copyCommand = `cp -r ${PACKS_EXAMPLES_DIRECTORY}/examples/template/* ${process.cwd()}`;
+  const copyCommand = `cp -r ${PacksExamplesDirectory}/examples/template/* ${process.cwd()}`;
   spawnProcess(copyCommand);
+  // npm removes .gitignore files when installing a package, so we can't simply put the .gitignore
+  // in the template example alongside the other files. So we just create it explicitly
+  // here as part of the init step.
+  const createIgnoreCommand = `echo "${GitIgnore}" > ${process.cwd()}/.gitignore`;
+  spawnProcess(createIgnoreCommand);
 
   if (!isPacksExamplesInstalled) {
     const uninstallCommand = `npm uninstall @codahq/packs-examples`;
