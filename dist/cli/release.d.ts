b1f9a179b782b6f1590a4f0295ca895dd0643cc0
Spencer Chang <spencer@coda.io>
2021-10-18 11:36:56 -0700

    make build

--- dist/cli/release.d.ts
+++
@@ -1,9 +0,0 @@
-import type { Arguments } from 'yargs';
-interface ReleaseArgs {
-    manifestFile: string;
-    packVersion?: string;
-    codaApiEndpoint: string;
-    notes?: string;
-}
-export declare function handleRelease({ manifestFile, packVersion: explicitPackVersion, codaApiEndpoint, notes, }: Arguments<ReleaseArgs>): Promise<never>;
-export {};
