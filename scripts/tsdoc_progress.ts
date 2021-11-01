6e0c29f150f0a6dc87c7d492d0aae41521d56285
Jonathan Goldman <50341122+jonathan-codaio@users.noreply.github.com>
2021-11-01 07:47:52 -0700

    Add a script that indicates which identifiers need TypeDoc documentation. (#1501)
    * Add a script that indicates which identifiers need TypeDoc documentation.
    
    * Exclude scripts from build.

---
+++ scripts/tsdoc_progress.ts
@@ -0,0 +1,53 @@
+import fs from 'fs';
+import os from 'os';
+import * as path from 'path';
+import {spawnSync} from 'child_process';
+
+/**
+ * Outputs all of the identifiers reachable by our TypeDoc config that are lacking documentation.
+ */
+
+// Very partial typing
+interface ReflectionData {
+  name: string;
+  kindString: string;
+  comment?: {
+    shortText: string;
+    text?: string;
+  };
+  sources?: Array<{
+    fileName: string;
+  }>;
+  children?: ReflectionData[];
+}
+
+function getReflectionData() {
+  const tempfile = path.join(os.tmpdir(), `typedoc.json`);
+  const command = `node_modules/.bin/typedoc index.ts --options typedoc.js --json ${tempfile} > /dev/null 2>&1`;
+  spawnSync(command, {
+    shell: true,
+    stdio: 'inherit',
+  });
+  return JSON.parse(fs.readFileSync(tempfile, 'utf-8'));
+}
+
+function traverse(data: ReflectionData): void {
+  if (!data.comment) {
+    logMissing(data);
+  }
+  for (const child of data.children || []) {
+    traverse(child);
+  }
+}
+
+function logMissing(data: ReflectionData): void {
+  // eslint-disable-next-line no-console
+  console.log(`${data.name.padEnd(40)} ${data.kindString.padEnd(30)} ${data.sources?.[0].fileName}`);
+}
+
+function main() {
+  const data = getReflectionData();
+  traverse(data);
+}
+
+main();
