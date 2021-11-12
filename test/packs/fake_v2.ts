4e43f31b13f17c7d1a2d094624a89485862dcea9
Huayang Guo <huayang@coda.io>
2021-11-12 12:43:11 -0800

    fix ambiguous execute return type

--- test/packs/fake_v2.ts
+++ test/packs/fake_v2.ts
@@ -2,9 +2,35 @@ import * as coda from '../..';
 
 export const pack = coda.newPack();
 
+const fakePersonSchema = coda.makeObjectSchema({
+  type: coda.ValueType.Object,
+  primary: 'name',
+  id: 'name',
+  properties: {
+    name: {type: coda.ValueType.String, required: true},
+  },
+});
+
 function doThrow() {
   throw new Error('test');
 }
+
+pack.addFormula({
+  name: 'Person',
+  description: 'A formula that returns an object',
+  parameters: [
+    coda.makeParameter({
+      type: coda.ParameterType.String,
+      name: 'name',
+      description: 'the name of the pereson',
+    }),
+  ],
+  resultType: coda.ValueType.Object,
+  schema: fakePersonSchema,
+  execute: async ([name]) => {
+    return {name};
+  },
+});
 
 pack.addFormula({
   name: 'Throw',
