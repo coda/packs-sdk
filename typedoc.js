0a7f65774d7afdcd8c4e8903d4e44d0e4514ca62
Jonathan Goldman <50341122+jonathan-codaio@users.noreply.github.com>
2021-11-17 14:25:44 -0800

    Treat TypeDoc warnings as errors so we enforce better coverage. (#1564)

--- typedoc.js
+++ typedoc.js
@@ -8,9 +8,43 @@ module.exports = {
   plugin: 'typedoc-plugin-markdown',
   // Don't include the repo's README in the generated docs.
   readme: 'none',
-  
+
   // Markdown-specific options.
   hideBreadcrumbs: true,
   hideInPageTOC: true,
   allReflectionsHaveOwnDocument: true,
+
+  intentionallyNotExported: [
+    // TODO(jonathan): Export and document these.
+    'AttributionNode',
+    'FormulaDefinitionV2',
+    'FormulaResultValueType',
+    'GenericObjectSchema',
+    'InvocationLocation',
+    'MetadataFunction',
+    'ObjectSchema',
+    'ObjectSchemaDefinitionType',
+    'ObjectSchemaType',
+    'RequestHandlerTemplate',
+    'ResponseHandlerTemplate',
+    'StatusCodeErrorResponse',
+    'ValidTypes',
+
+    // Internal intermediate or helper types that we probably don't care to export or document.
+    '$Values',
+    'AsAuthDef',
+    'ParameterTypeMap',
+    'SimpleStringHintTypes',
+    'StringHintTypeToSchemaType',
+    'TypeMap',
+    'TypeOf',
+    'TypeOfMap',
+    'UnionType',
+
+    // Deprecated features we should remove from the SDK
+    'FeatureSet',
+    'PackCategory',
+    'Quota',
+    'RateLimits',
+  ],
 };
