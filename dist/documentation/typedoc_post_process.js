a7900ab80bbd67c3d7db3fb2560add74e7637c10
Eric Koleda <ekoleda@coda.io>
2021-12-22 20:30:21 -0500

    Rebuild

---
+++ dist/documentation/typedoc_post_process.js
@@ -0,0 +1,75 @@
+"use strict";
+var __createBinding = (this && this.__createBinding) || (Object.create ? (function(o, m, k, k2) {
+    if (k2 === undefined) k2 = k;
+    Object.defineProperty(o, k2, { enumerable: true, get: function() { return m[k]; } });
+}) : (function(o, m, k, k2) {
+    if (k2 === undefined) k2 = k;
+    o[k2] = m[k];
+}));
+var __setModuleDefault = (this && this.__setModuleDefault) || (Object.create ? (function(o, v) {
+    Object.defineProperty(o, "default", { enumerable: true, value: v });
+}) : function(o, v) {
+    o["default"] = v;
+});
+var __importStar = (this && this.__importStar) || function (mod) {
+    if (mod && mod.__esModule) return mod;
+    var result = {};
+    if (mod != null) for (var k in mod) if (k !== "default" && Object.prototype.hasOwnProperty.call(mod, k)) __createBinding(result, mod, k);
+    __setModuleDefault(result, mod);
+    return result;
+};
+var __importDefault = (this && this.__importDefault) || function (mod) {
+    return (mod && mod.__esModule) ? mod : { "default": mod };
+};
+Object.defineProperty(exports, "__esModule", { value: true });
+const fs = __importStar(require("fs"));
+const glob_1 = __importDefault(require("glob"));
+const path_1 = __importDefault(require("path"));
+const helpers_1 = require("../testing/helpers");
+const BaseDir = path_1.default.join(__dirname, '..');
+const TypeDocsRoot = path_1.default.join(BaseDir, 'docs/reference/sdk');
+const MarkdownPattern = path_1.default.join(TypeDocsRoot, '**/*.md');
+// Matches "Type: Name" and "Type: Name<Template Stuff>".
+const TitleRegex = /^# .+?\:\s*(.*?)(?:<.+>)?$/m;
+async function main() {
+    const files = await getFiles();
+    const promises = [];
+    for (const file of files) {
+        promises.push(process(file));
+    }
+    await Promise.all(promises);
+}
+async function getFiles() {
+    return new Promise((resolve, reject) => {
+        (0, glob_1.default)(MarkdownPattern, {}, (err, files) => {
+            if (err) {
+                reject(err);
+            }
+            resolve(files);
+        });
+    });
+}
+async function process(file) {
+    return addFrontmatter(file);
+}
+/**
+ * Adds frontmatter to generated markdown files, setting a simplified title. The
+ * frontmatter title is used in the nav.
+ */
+async function addFrontmatter(file) {
+    const buf = await fs.promises.readFile(file);
+    const content = buf.toString();
+    if (content.startsWith('---\n')) {
+        (0, helpers_1.print)(`Already has frontmatter: ${file}`);
+        return;
+    }
+    const match = content.match(TitleRegex);
+    if (!match) {
+        (0, helpers_1.print)(`Title doesn't need fixing: ${file}`);
+        return;
+    }
+    const title = match[1];
+    const frontmatter = `---\ntitle: ${title}\n---\n`;
+    return fs.promises.writeFile(file, frontmatter + content);
+}
+main().catch(helpers_1.print);
