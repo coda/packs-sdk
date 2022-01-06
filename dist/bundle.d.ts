54e4c759544d5435f0e35c6cb759920dae85415a
Patrick Barry <patrick@coda.io>
2022-01-05 16:25:39 -0800

    add oauth fields that security-minded end users might want to see into ExternalPackVersionMetadata

--- dist/bundle.d.ts
+++ dist/bundle.d.ts
@@ -3478,6 +3478,8 @@ export interface ExternalPackVersionMetadata extends BasePackVersionMetadata {
 		postSetup?: PostSetupMetadata[];
 		deferConnectionSetup?: boolean;
 		shouldAutoAuthSetup?: boolean;
+		oauthScopes?: string[];
+		oauthAuthorizationUrl?: string;
 	};
 	instructionsUrl?: string;
 	formulas?: ExternalPackFormulas;
