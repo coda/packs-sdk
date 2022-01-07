3fc988b0fa55a1fbebce1409b88d0edaa5957e09
Spencer Chang <spencer@coda.io>
2022-01-07 08:29:50 -0800

    remove extraneous "packs" in url

--- documentation/documentation_publisher.ts
+++ documentation/documentation_publisher.ts
@@ -17,7 +17,9 @@ const DocumentationRoot = '/packs/build';
 const AwsRegion = 'us-west-2';
 const BaseGeneratedDocsPath = 'site';
 const DocumentationBucket = 'developer-documentation';
-const PacksSdkBucketRootPath = 'packs';
+// NOTE(spencer): we are getting rid of the redundant "packs"
+// bucket, so there is no root path outside of the bucket above.
+// const PacksSdkBucketRootPath = 'packs';
 
 function handleError(e: Error) {
   printAndExit(`${e.message} ${e.stack || ''}`);
@@ -58,11 +60,11 @@ function getSDKVersion(): string {
 }
 
 function getS3DocVersionedKey(): string {
-  return `${PacksSdkBucketRootPath}/${getSDKVersion()}`;
+  return `${getSDKVersion()}`;
 }
 
 function getS3LatestDocsKey(): string {
-  return `${PacksSdkBucketRootPath}/latest`;
+  return `latest`;
 }
 
 interface PushDocumentationArgs {
