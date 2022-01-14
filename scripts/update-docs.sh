1ba0592746b1ecb3a342cdfa87aeee84be29f40f
Spencer Chang <spencer@coda.io>
2022-01-14 12:00:28 -0800

    add script + update readme

---
+++ scripts/update-docs.sh
@@ -0,0 +1,19 @@
+###############################################################################
+# This is temporary script to update the docs while our CI is disabled due to 
+# security.
+###############################################################################
+
+# head
+MK_DOCS_SITE_URL="https://head.coda.io/packs/build/latest/" make build-mkdocs
+make publish-docs-head FLAGS=--forceUpload
+
+# staging
+MK_DOCS_SITE_URL="https://staging.coda.io/packs/build/latest/" make build-mkdocs
+make publish-docs-staging FLAGS=--forceUpload
+
+# prod
+MK_DOCS_SITE_URL="https://coda.io/packs/build/latest/" make build-mkdocs
+make publish-docs-prod FLAGS=--forceUpload
+
+echo "\nFinishing pushing to all environments!"
+echo "Run `make npm-publish` to publish to npm"
