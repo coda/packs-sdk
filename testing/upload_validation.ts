d84c849ff417be7ceb0b5e84e6a508cc32f657a9
Jonathan Goldman <jonathan@coda.io>
2022-04-26 18:56:06 +0000

    Don't hard fail on breaking changes until future version 1.0.0.

--- testing/upload_validation.ts
+++ testing/upload_validation.ts
@@ -1227,8 +1227,8 @@ interface SchemaExtension {
 const packMetadataSchemaBySdkVersion: SchemaExtension[] = [
   {
     // Check that packs with multiple network domains explicitly choose which domain gets auth.
-    // This is a backward-incompatible validation that takes effect in any pack release after 0.9.0.
-    versionRange: '>0.9.0',
+    // This is a backward-incompatible validation that takes effect in any pack release after 1.0.0.
+    versionRange: '>=1.0.0',
     schemaExtend: schema => {
       return schema.superRefine((untypedData, context) => {
         const data = untypedData as PackVersionMetadata;
@@ -1272,7 +1272,7 @@ const packMetadataSchemaBySdkVersion: SchemaExtension[] = [
     },
   },
   {
-    versionRange: '>0.9.0',
+    versionRange: '>=1.0.0',
     schemaExtend: schema => {
       return schema.superRefine((untypedData, context) => {
         const data = untypedData as PackVersionMetadata;
