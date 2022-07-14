90fcae7fd743dd989646f4ea1d85dff4c5ac575a
David Weitzman <dweitzman@coda.io>
2022-07-14 10:39:25 -0700

    Comment on why we avoid object copies where possible

--- runtime/common/marshaling/index.ts
+++ runtime/common/marshaling/index.ts
@@ -115,6 +115,10 @@ function fixUncopyableTypes(
         hadModifications = true;
       }
     }
+    // We don't want to accidentally replace something like a Date object with a simple
+    // object, so we only return a copied version if we actually discover a buffer within.
+    // Another option here might be to check against a known list of types which structuredClone()
+    // supports and skip all this copy logic for known-safe types.
     if (hadModifications) {
       return {val: maybeModifiedObject, hasModifications: true};
     }
