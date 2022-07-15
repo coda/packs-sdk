416ce890b451c436d67bfb8087b37ae378ec608a
David Weitzman <dweitzman@coda.io>
2022-07-15 15:21:46 -0700

    Add a comment about the thunk compilation hackery

--- dist/runtime/common/marshaling/serializer.js
+++ dist/runtime/common/marshaling/serializer.js
@@ -5,6 +5,13 @@ let serialize;
 exports.serialize = serialize;
 let deserialize;
 exports.deserialize = deserialize;
+// Make sure dead code elimination removes the require('v8'), which won't be able to
+// compile in the runtime thunk. We have --define:process.env.IS_THUNK=true in the
+// Makefile that activates the thunk-specific code.
+//
+// NOTE(dweitzman): The use of process.env here feels a little weird, but I haven't found
+// another way to ensure that esbuild won't complain about undeclared variables or try
+// to include the 'v8' module in the thunk.
 if (process.env.IS_THUNK) {
     exports.serialize = serialize = codaInternal.serializer.serialize;
     exports.deserialize = deserialize = codaInternal.serializer.deserialize;
--- runtime/common/marshaling/serializer.ts
+++ runtime/common/marshaling/serializer.ts
@@ -10,6 +10,13 @@ declare const codaInternal: CodaInternal;
 let serialize: (value: any) => string;
 let deserialize: (buffer: string) => any;
 
+// Make sure dead code elimination removes the require('v8'), which won't be able to
+// compile in the runtime thunk. We have --define:process.env.IS_THUNK=true in the
+// Makefile that activates the thunk-specific code.
+//
+// NOTE(dweitzman): The use of process.env here feels a little weird, but I haven't found
+// another way to ensure that esbuild won't complain about undeclared variables or try
+// to include the 'v8' module in the thunk.
 if (process.env.IS_THUNK) {
   serialize = codaInternal.serializer.serialize;
   deserialize = codaInternal.serializer.deserialize;
