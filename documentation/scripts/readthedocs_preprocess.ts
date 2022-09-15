37d58918967ea36b723ec981f1aa568dfc2142a7
Eric Koleda <ekoleda@coda.io>
2022-09-15 13:07:48 -0400

    Refactor rtd script

--- documentation/scripts/readthedocs_preprocess.ts
+++ documentation/scripts/readthedocs_preprocess.ts
@@ -1,3 +1,8 @@
+/**
+ * Pre-processing script that runs before Read the Docs generates the documentation.
+ * See .readthedocs.yaml for where it is executed.
+ */
+
 import fs from 'fs';
 import { print } from '../../testing/helpers';
 import * as yaml from 'js-yaml';
@@ -67,22 +72,41 @@ const UnknownYamlTypes = [ 'scalar', 'sequence', 'mapping' ].map(kind => {
   });
 });
 
-const schema = yaml.DEFAULT_SCHEMA.extend([
-  EnvScalarYamlType,
-  EnvSequenceYamlType,
-  ...UnknownYamlTypes
-]);
-
-// Manually set the site_url, since ReadTheDocs can't load environment variables in mkdocs.yml.
-// https://github.com/readthedocs/readthedocs.org/issues/8529#issuecomment-929063527
-let siteUrl = `https://${process.env.READTHEDOCS_PROJECT}.readthedocs-hosted.com/${process.env.READTHEDOCS_LANGUAGE}/${process.env.READTHEDOCS_VERSION}/`;
-if (process.env.READTHEDOCS_VERSION_TYPE === 'external') {
-  siteUrl = `https://${process.env.READTHEDOCS_PROJECT}--${process.env.READTHEDOCS_VERSION}.com.readthedocs.build/en/${process.env.READTHEDOCS_VERSION}/`;
+/**
+ * Read the Docs generates a unique URL for each version / PR, so you can't set a static value for MK_DOCS_SITE_URL.
+ * They don't expose a way to access the full generated URL, but using other environment variables you can reconstruct
+ * it. This helper function generates that URL and saves it to MK_DOCS_SITE_URL.
+ *
+ *
+ * @see {@link https://github.com/readthedocs/readthedocs.org/issues/8260}
+ */
+function setSiteUrl() {
+  let siteUrl = `https://${process.env.READTHEDOCS_PROJECT}.readthedocs-hosted.com/${process.env.READTHEDOCS_LANGUAGE}/${process.env.READTHEDOCS_VERSION}/`;
+  if (process.env.READTHEDOCS_VERSION_TYPE === 'external') {
+    siteUrl = `https://${process.env.READTHEDOCS_PROJECT}--${process.env.READTHEDOCS_VERSION}.com.readthedocs.build/en/${process.env.READTHEDOCS_VERSION}/`;
+  }
+  process.env.MK_DOCS_SITE_URL = siteUrl;
+  print(`Set MK_DOCS_SITE_URL to: ${siteUrl}`);
 }
-process.env.MK_DOCS_SITE_URL = siteUrl;
-print(`Set MK_DOCS_SITE_URL to: ${siteUrl}`);
 
-let content = fs.readFileSync(FilePath, 'utf8');
-const doc = yaml.load(content, {schema});
-content = yaml.dump(doc, {schema});
-fs.writeFileSync(FilePath, content);
+/**
+ * Read the Docs doesn't handle the !ENV tag in mkdocs.yml that loads values from environment variables. This helper
+ * script manually replaces !ENV tags with the corresponding environment variable values and saves the file back.
+ *
+ * @see {@link https://github.com/readthedocs/readthedocs.org/issues/8529#issuecomment-929063527}
+ */
+function replaceEnvVars() {
+  const schema = yaml.DEFAULT_SCHEMA.extend([
+    EnvScalarYamlType,
+    EnvSequenceYamlType,
+    ...UnknownYamlTypes
+  ]);
+
+  let content = fs.readFileSync(FilePath, 'utf8');
+  const doc = yaml.load(content, {schema});
+  content = yaml.dump(doc, {schema});
+  fs.writeFileSync(FilePath, content);
+}
+
+setSiteUrl();
+replaceEnvVars();
