4761a6686122108001bdb4b8d460e33d0fbb3752
Spencer Chang <spencer@coda.io>
2022-09-26 14:23:20 -0700

    update export

--- index.ts
+++ index.ts
@@ -173,6 +173,7 @@ export type {ObjectSchemaDefinition} from './schema';
 export type {ObjectSchemaProperties} from './schema';
 export type {ObjectSchemaProperty} from './schema';
 export type {ProgressBarSchema} from './schema';
+export type {PropertyType} from './schema';
 export {ScaleIconSet} from './schema';
 export type {ScaleSchema} from './schema';
 export type {Schema} from './schema';
