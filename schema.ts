2b4cd1d8d5b17697887c001749568539b6af0309
Spencer Chang <spencer@coda.io>
2022-09-26 12:54:15 -0700

    update the type

--- schema.ts
+++ schema.ts
@@ -839,6 +839,14 @@ export interface Identity extends IdentityDefinition {
   packId: number;
 }
 
+interface ObjectProperty {
+  label: string;
+  path: string;
+  showLabel?: boolean;
+}
+
+export type PropertyType<K extends string> = K | ObjectProperty;
+
 /**
  * A schema definition for an object value (a value with key-value pairs).
  */
@@ -920,7 +928,7 @@ export interface ObjectSchemaDefinition<K extends string, L extends string> exte
    * Must be a {@link ValueType.String} property
    */
   /** @hidden */
-  titleProperty?: L;
+  titleProperty?: PropertyType<K>;
   /**
    * The name of a property within {@link ObjectSchemaDefinition.properties} that will
    * navigate users to more details about this object
@@ -929,14 +937,14 @@ export interface ObjectSchemaDefinition<K extends string, L extends string> exte
    * {@link ObjectSchemaDefinition.codaType}.
    */
   /** @hidden */
-  linkProperty?: L;
+  linkProperty?: PropertyType<K>;
   /**
    * A list of property names from within {@link ObjectSchemaDefinition.properties} for the properties of the object
    * to be shown in the subtitle of a rich card preview for formulas that return this object.
    * Defaults to the value of {@link ObjectSchemaDefinition.featuredProperties} if not specified.
    */
   /** @hidden */
-  subtitleProperties?: L[];
+  subtitleProperties?: Array<PropertyType<K>>;
   /**
    * The name of a property within {@link ObjectSchemaDefinition.properties} that be used as a long body description
    * of the object.
@@ -944,7 +952,7 @@ export interface ObjectSchemaDefinition<K extends string, L extends string> exte
    * Must be a {@link ValueType.String} property or {@link ValueType.Array} of {@link ValueType.String}s.
    */
   /** @hidden */
-  descriptionProperty?: L;
+  descriptionProperty?: PropertyType<K>;
   /**
    * The name of a property within {@link ObjectSchemaDefinition.properties} that can be used as a rich image preview of
    * the object.
@@ -953,7 +961,7 @@ export interface ObjectSchemaDefinition<K extends string, L extends string> exte
    * {@link ValueHintType.ImageAttachment} or {@link ValueHintType.ImageReference} hints
    */
   /** @hidden */
-  imageProperty?: L;
+  imageProperty?: PropertyType<K>;
 }
 
 export type ObjectSchemaDefinitionType<
