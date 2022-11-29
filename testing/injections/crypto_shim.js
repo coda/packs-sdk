f30584098808539bac326061d7ab0b0743c71d00
Alan Fang <alan@coda.io>
2022-11-29 16:07:16 -0600

    fix

--- testing/injections/crypto_shim.js
+++ testing/injections/crypto_shim.js
@@ -36,7 +36,7 @@ export const crypto = {
 //
 // please note that this causes a global leak and needs be ignored in some configs.
 // Node 19 has native support for the crypto module.
-const version = process.version.substring(1); // strip out first char from v19.x.x
+const version = process?.version.substring(1); // strip out first char from v19.x.x
 if (semver.valid(version) && semver.compare(version, '19.0.0') < 0) {
   global.crypto = crypto;
 }
