102837bd548fb43c7535f6c58d6d0a023eb55d00
Alan Fang <alan@coda.io>
2022-11-29 15:58:09 -0600

    fix

--- dist/testing/injections/crypto_shim.js
+++ dist/testing/injections/crypto_shim.js
@@ -35,7 +35,8 @@ export const crypto = {
 // It has the same side effect of the shim though.
 //
 // please note that this causes a global leak and needs be ignored in some configs.
-// Node 19 has native support for crypto module.
-if (semver.compare(process.version, '19.0.0') < 0) {
+// Node 19 has native support for the crypto module.
+const version = process.version.substring(1); // strip out first char from v19.x.x
+if (semver.valid(version) && semver.compare(version, '19.0.0') < 0) {
   global.crypto = crypto;
 }
