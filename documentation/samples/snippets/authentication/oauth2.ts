62c9204e2284b4f89bae74dceba2f8bd98fea455
Eric Koleda <ekoleda@coda.io>
2022-11-30 16:06:32 -0500

    A few more fixes

--- documentation/samples/snippets/authentication/oauth2.ts
+++ documentation/samples/snippets/authentication/oauth2.ts
@@ -5,7 +5,7 @@ const pack = coda.newPack();
 // BEGIN
 
 // When registering your app in the API's developer console set the redirect URL
-// to: https://coda.io/packsAuth/oauth2
+// to: https://coda.io/packsAuth/oauth2/{PACK ID}
 // After building your Pack, remember to visit the Settings tab to set your
 // client ID and secret.
 pack.setUserAuthentication({
