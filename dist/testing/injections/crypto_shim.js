34a344c235fd76314649063b565589e61d26d1d9
Alan Fang <79662144+alan-codaio@users.noreply.github.com>
2023-02-15 11:51:17 -0500

    don't import our crypto shim if crypto is supported natively (#2350)
    * don't import our crypto shim if node version > 19.0.0
    
    * fix
    
    * fix
    
    * fix tests
    
    * dont check by version

--- dist/testing/injections/crypto_shim.js
+++ dist/testing/injections/crypto_shim.js
@@ -1,37 +1,41 @@
 // ivm doens't have a crypto implementation. since we browserify modules already, this shim implements the browser crypto interface.
 // inspired by https://github.com/kumavis/polyfill-crypto.getrandomvalues/blob/master/index.js
 
-var MersenneTwister = require('mersenne-twister')
+var MersenneTwister = require('mersenne-twister');
+var semver = require('semver');
 
-var twister = new MersenneTwister(Math.random() * Number.MAX_SAFE_INTEGER)
+var twister = new MersenneTwister(Math.random() * Number.MAX_SAFE_INTEGER);
 
-function getRandomValues (abv) {
-  var l = abv.length
+function getRandomValues(abv) {
+  var l = abv.length;
   while (l--) {
-    abv[l] = Math.floor(randomFloat() * 256)
+    abv[l] = Math.floor(randomFloat() * 256);
   }
-  return abv
+  return abv;
 }
 
 function randomFloat() {
-  return twister.random()
+  return twister.random();
 }
 
 export const crypto = {
   getRandomValues,
-}
+};
 
-// esbuild isn't injecting the shim exports into global. in this particular case, crypto 
-// library is usually used as global.crypto which returns undefined otherwise. 
-// 
+// esbuild isn't injecting the shim exports into global. in this particular case, crypto
+// library is usually used as global.crypto which returns undefined otherwise.
+//
 // alternatively a few other approaches are tried:
-// - shim global: which doesn't work with VM somehow since the VM manages context.global which 
+// - shim global: which doesn't work with VM somehow since the VM manages context.global which
 //   seems a different object from the global here. causing manifest to be undefined.
-// - use esbuild define global.crypto: crypto. didn't work. 
+// - use esbuild define global.crypto: crypto. didn't work.
 // - https://github.com/evanw/esbuild/issues/296 Didn't work since we banned eval in VM.
 //
 // Lastly, we can move this shim to thunk bundle and register it into global from the thunk bundle.
-// It has the same side effect of the shim though. 
+// It has the same side effect of the shim though.
 //
 // please note that this causes a global leak and needs be ignored in some configs.
-global.crypto = crypto;
+// Node 19 has native support for the crypto module.
+if (!global.crypto?.getRandomValues) {
+  global.crypto = crypto;
+}
