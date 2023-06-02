0c1ab2a1931b6a15c865bbc28276766b1165ea68
David Weitzman <dweitzman@coda.io>
2023-06-02 13:48:39 -0700

    Maybe this works?

--- scripts/dev/install-pnpm.sh
+++ scripts/dev/install-pnpm.sh
@@ -3,4 +3,4 @@
 # We can't install pnpm using node because pnpm 8 isn't compatible with node 14, which is what
 # we use on lambda. Instead, we can install a standalone binary that doesn't depend on node.
 
-npm install -g @pnpm/exe@~8.5.1
+sudo npm install -g @pnpm/exe@~8.5.1
