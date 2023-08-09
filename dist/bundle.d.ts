4f9249c1e386f237d4e1e928a8716b3c1026ad03
Gary Shen <100240943+gary-codaio@users.noreply.github.com>
2023-08-09 14:02:37 -0700

    make endpoint key available in external metadata (#2701)

--- dist/bundle.d.ts
+++ dist/bundle.d.ts
@@ -4560,6 +4560,7 @@ export interface ExternalPackVersionMetadata extends BasePackVersionMetadata {
 		oauthAuthorizationUrl?: string;
 		oauthTokenUrl?: string;
 		networkDomain?: string | string[];
+		endpointKey?: string;
 	};
 	instructionsUrl?: string;
 	formulas?: ExternalPackFormulas;
