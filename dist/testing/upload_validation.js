1a36598b64dfdf6f3af1b0f91a27020aca4e82be
Patrick Barry <patrick@coda.io>
2024-03-10 15:03:34 -0700

    buiold

--- dist/testing/upload_validation.js
+++ dist/testing/upload_validation.js
@@ -1281,7 +1281,7 @@ function buildMetadataSchema({ sdkVersion }) {
     const unrefinedPackVersionMetadataSchema = zodCompleteObject({
         version: z
             .string()
-            .regex(/^\d+(\.\d+){0,2}$/, 'Pack versions must use semantic versioning, e.g. "1", "1.0" or "1.0.0".')
+            .regex(/^\d+(\.\d+){0,2}(\-prerelease\.\d+)?$/, 'Pack versions must use semantic versioning, e.g. "1", "1.0" or "1.0.0".')
             .refine(
         // Version numbers must not be bigger than a postgres integer.
         version => version.split('.').filter(part => Number(part) > 2147483647).length === 0, 'Pack version number too large'),
