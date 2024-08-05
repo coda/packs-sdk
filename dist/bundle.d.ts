17b63edc39ae2fa21ec7893ba0273509d9fe6f2f
krypton-build-user <build-notifications@krypton.io>
2024-08-05 11:16:50 -0700

    Update dependency dts-bundle-generator to v9 (#2997)
    Co-authored-by: Renovate Bot <renovate@whitesourcesoftware.com>

--- dist/bundle.d.ts
+++ dist/bundle.d.ts
@@ -5024,6 +5024,10 @@ export declare function withQueryParams(url: string, params?: {
 export declare function getQueryParams(url: string): {
 	[key: string]: any;
 };
+/**
+ * Joins all the tokens into a single URL string separated by '/'. Zero length tokens cause errors.
+ * @param tokens Zero or more tokens to be combined. If token doesn't end with '/', one will be added as the separator
+ */
 declare function join(...tokens: string[]): string;
 /**
  * A helper to extract properties fromKeys from a schema object. This is mostly useful
