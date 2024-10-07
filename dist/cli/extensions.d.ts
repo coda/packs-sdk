2db923b25c3735e382063790dbfdbd03b5dac06f
Sam Sloate <sam@coda.io>
2024-10-07 10:57:43 -0400

    build

--- dist/cli/extensions.d.ts
+++
@@ -1,9 +0,0 @@
-import type { ArgumentsCamelCase } from 'yargs';
-export declare enum Tools {
-    VSCode = "vscode"
-}
-interface ExtensionsArgs {
-    tools: Tools[];
-}
-export declare function handleExtensions(args: ArgumentsCamelCase<ExtensionsArgs>): Promise<void>;
-export {};
--- dist/runtime/common/marshaling/serializer.d.ts
+++
@@ -1,3 +0,0 @@
-declare let serialize: (value: any) => string;
-declare let deserialize: (buffer: string) => any;
-export { serialize, deserialize };
