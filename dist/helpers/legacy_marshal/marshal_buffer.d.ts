ed0fa0ea4ffec8bc83f9b07fabef268b31d50007
Patrick Barry <patrick@coda.io>
2025-02-27 12:58:14 -0800

    rebuild

--- dist/helpers/legacy_marshal/marshal_buffer.d.ts
+++ dist/helpers/legacy_marshal/marshal_buffer.d.ts
@@ -1,4 +1,5 @@
 /// <reference types="node" />
+/// <reference types="node" />
 export declare function marshalBuffer(val: any): object | undefined;
 export declare function unmarshalBuffer(val: {
     [key: string]: any;
