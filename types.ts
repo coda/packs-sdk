6e9a0e4c924462ed7f1f91aaf11c3b317fa5cfb7
denis.zavgorodny <denys.zavhorodnii@grammarly.com>
2025-08-04 17:43:15 +0300

    add description

--- types.ts
+++ types.ts
@@ -1191,6 +1191,7 @@ export enum ToolType {
   ScreenAnnotation = 'ScreenAnnotation',
   /**
    * Allows assistant messages to be used as tools.
+   * This tool is for internal usage only.
    * @hidden
    */
   AssistantMessage = 'AssistantMessage',
