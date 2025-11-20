ff6941f12046ad5a62cadb820343c2803a478860
Wesley Hsieh <wesley@coda.io>
2025-11-20 15:51:47 -0500

    Update types.ts (#3356)

--- types.ts
+++ types.ts
@@ -1241,8 +1241,6 @@ export interface PackTool extends BaseTool<ToolType.Pack> {
   formulas?: Array<{
     /** The name of the formula to use as a tool. */
     formulaName: string;
-    /** Instructions for LLMs to use the formula. */
-    instructions?: string;
   }>;
 }
 
