cd2b32e92b0d1223cd043812b41e6a507f22ca23
stanislavstudzinskyi-codaio <stanislav.studzinskyi@grammarly.com>
2025-11-21 14:14:37 -0800

    forcedFunction => forcedFormula

--- dist/types.d.ts
+++ dist/types.d.ts
@@ -1286,8 +1286,8 @@ export interface Skill {
     prompt: string;
     /** List of tools that this skill can use. This does NOT include pack calls by default. */
     tools: Tool[];
-    /** Forces execution of a specific tool by name, overriding autonomous tool selection. */
-    forcedFunction?: string;
+    /** Forces execution of a specific formula by name, overriding autonomous tool selection. */
+    forcedFormula?: string;
 }
 /**
  * Configuration for a skill entrypoint.
