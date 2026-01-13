e8feb4ea52be6e8f591b88d6b827c2999c3419cf
Jonathan Goldman <jonathan.goldman@grammarly.com>
2026-01-13 10:29:05 -0800

    Fix nvm env setup to allow for properly building isolated-vm.

--- setup-env.sh
+++ setup-env.sh
@@ -12,8 +12,11 @@ if command -v nvm &> /dev/null
 then
   # Gets version from .nvmrc
   nvm use
+  # Ensure nvm's node is at the front of PATH (override Homebrew node)
+  if [[ -n "$NVM_BIN" ]]; then
+    export PATH="${NVM_BIN}:${PATH}"
+  fi
 fi
-
 
 export PATH=${BASEDIR}/.pnpm_install/bin:${BASEDIR}/build/node/bin:${BASEDIR}/node_modules/.bin:${PATH}
 export NODE_OPTIONS="--no-node-snapshot ${NODE_OPTIONS}"
