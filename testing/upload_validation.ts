bca0cfe2fd66da3025f21885e143381c4394abb6
yevheniisabanin <169062018+yevheniisabanin@users.noreply.github.com>
2026-05-13 15:23:11 +0200

    Allow more props for outlook required fro Notetaker (#3545)
    * Allow more props for outlook required fro Notetaker
    
    * fix missing version bump
    
    * run make build
    
    * Revert local-built dist/bundle.js (CI generates the canonical Linux build)

--- testing/upload_validation.ts
+++ testing/upload_validation.ts
@@ -1429,6 +1429,7 @@ ${endpointKey ? 'endpointKey is set' : `requiresEndpointUrl is ${requiresEndpoin
     [1013, 'Sync pull requests', ExemptionType.SyncTableGetterName],
     [1013, 'Sync repos', ExemptionType.SyncTableGetterName],
     [1003, 'Event', ExemptionType.FilterablePropertyLimit],
+    [42226, 'Event', ExemptionType.FilterablePropertyLimit],
     [1052, 'Issue', ExemptionType.FilterablePropertyLimit],
     [1054, 'Sync table', ExemptionType.SyncTableGetterName],
     [1062, 'Form responses', ExemptionType.SyncTableGetterName],
